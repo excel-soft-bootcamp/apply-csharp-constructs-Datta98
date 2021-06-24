3f8adefcd91639854e49b67789a83752d3e0d314
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-06-24 17:24:54 +0530

    Add files via upload

---
+++ csharp essentials/csharp essentials/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace csharp_essentials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            customer obj1 = new customer("Tom", "6578-4567-98760-5678", "ADVPV00T");
+            customer obj2 = new customer("Tom", "6578-4567-98760-5678", "ADVPV00T", "1234566");
+            customer obj3 = new customer("Tom", "6578-4567-98760-5678", "ADVPV00T", "1234566", "info@infi.com");
+
+         
+           
+        }
+    }
+}
---
+++ csharp essentials/csharp essentials/constructorOverloading.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp_essentials
+{
+    class customer
+
+    {
+        string name, id, address, contactNumber, adharNumber, pancardNumber, emailId;
+
+        public customer ( string name, string adhar, string pancard)
+        {
+            this.name = name;
+            this.adharNumber = adhar;
+            this.pancardNumber = pancard;
+        }
+        
+        public customer (string name, string adhar, string pancard, string contactNumber): this(name,adhar, pancard)
+        {
+            this.contactNumber = contactNumber;
+        }
+
+        public customer ( string name, string adhar, string pancard, string contactNumber, string emailId): this(name,adhar,pancard,contactNumber)
+        {
+            this.emailId = emailId;
+        }
+         
+    }
+}
