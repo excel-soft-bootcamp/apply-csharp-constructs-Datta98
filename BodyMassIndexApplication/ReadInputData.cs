8ab2f63159af59dfa9df8104162b268f162bcadc
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-06-25 14:43:55 +0530

    Add files via upload

---
+++ BodyMassIndexApplication/ReadInputData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BodyMassIndexApplication
+{
+    class ReadInputData
+    {
+        public double InputHeight()
+        {
+            System.Console.WriteLine("Enter your height in meters");
+            double Height = Convert.ToDouble((Console.ReadLine());
+            return Height;
+        }
+        public double InputWeight()
+        {
+            System.Console.WriteLine("Enter your Weight in Kg's ");
+            double Weight = Convert.ToDouble((Console.ReadLine());
+            return Weight;
+        }
+
+    }
+}
