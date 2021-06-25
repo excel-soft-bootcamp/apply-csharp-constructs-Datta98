2f5137849dc2fd906dc850349920317ab72799a6
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-06-25 14:41:23 +0530

    Add files via upload

---
+++ BodyMassIndexApplication/BmiCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BodyMassIndexApplication
+{
+    class BmiCalculator
+    {
+
+        private double height;
+        private double weight;
+        private double result;
+
+       
+        public double  BmiCalculation( double height, double weight)
+        {
+            this.height = height;
+            this.weight = weight;
+
+            this.result = this.weight / this.height;
+            return this.result;
+
+                
+        }
+       
+    }
+}
---
+++ BodyMassIndexApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BodyMassIndexApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadInputData inputdata = new ReadInputData();
+            double height = inputdata.InputHeight();
+            double weight = inputdata.InputWeight();
+
+            BmiCalculator bmi = new BmiCalculator(height, weight);
+            double result = bmi.BmiCalculation();
+
+            ResultDisplay result = new ResultDisplay(result);
+            result
+                .Display();
+
+        }
+    }
+}
