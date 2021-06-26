c1196896b0dec5e228878072140deaa30ac27b9a
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-06-26 14:33:02 +0530

    Add files via upload

---
+++ CampusEntryApplication/Guest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CampusEntryApplication
+{
+    public class Guest : Person
+    {
+
+        string _appointmentby;
+        public Guest(string name, String dob, string mobileno, string appiontmentby, string address) : base(name, dob, mobileno, address)
+        {
+            this._appointmentby = appiontmentby;
+        }
+    }
+}
