b77265999a7925fdfb9adfd17c5547248d6efb79
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-06-28 09:09:10 +0530

    Add files via upload

---
+++ CarProduction/TATACar.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarProduction
+{
+    class TATACar
+    {
+
+        IEngine _engine;
+      
+        public void TataCar(IEngine engine)
+        {
+
+            this._engine = engine;
+        }
+
+        public void Drive()
+        {
+
+           _engine.Start();
+        }
+        public void Halt()
+        {
+            
+            _engine.Stop();
+        }
+
+    }
+    public interface IEngine
+    {
+        void Start();
+        void Stop();
+    }
+
+
+
+    public class DiCorEngine : IEngine
+    {
+      
+        public void Start() {  }
+        public void Stop() {  }
+    }
+
+    public class VeriCorEngine : IEngine
+    {
+        public void Start() {  }
+        public void Stop() {  }
+
+    }
+
+    public class MultiJetEngine : IEngine
+    {
+        public void Start() {  }
+        public void Stop() {  }
+    }
+    public class RevorTron : IEngine
+    {
+        public void Start() {  }
+        public void Stop() {  }
+    }
+
+}
