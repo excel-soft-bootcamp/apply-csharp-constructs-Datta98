f9d1188291f6c9d088db26554c96b1bd047b02c7
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-07-06 15:57:36 +0530

    Add files via upload

---
+++ BMICalculator/BMIValueCalculationLib/BmiValueCalculation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BMIValueCalculationLib
+{
+    public class BmiValueCalculation
+    {
+
+        private double bmivalue;
+
+        public double calculator(double height, double weight)
+        {
+            bmivalue = weight / height;
+            return bmivalue;
+        }
+
+    }
+}
