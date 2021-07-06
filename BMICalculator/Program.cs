3283239ec012e365ec2b583bbcacb87d6fe4f9f7
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-07-06 15:53:09 +0530

    Add files via upload

---
+++ BMICalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BMICalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BMIUserInputLib.UserInputs userInputs = new BMIUserInputLib.UserInputs();
+
+
+            BMIValueCalculationLib.BmiValueCalculation value = new BMIValueCalculationLib.BmiValueCalculation();
+            double BmiValue = value.calculator(userInputs.UserHeight(), userInputs.UserWeight());
+
+            BMIValueValidationLib.BmiValueValidation validate = new BMIValueValidationLib.BmiValueValidation();
+            string message = validate.CheckCondition(BmiValue);
+
+            BMIResultDisplayLib.MessageDisplay result = new BMIResultDisplayLib.MessageDisplay();
+            result.Display(message);
+            Console.ReadKey();
+
+        }
+    }
+}
