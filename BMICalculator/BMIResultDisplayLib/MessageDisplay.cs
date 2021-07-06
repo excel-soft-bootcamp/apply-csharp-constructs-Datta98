13a021064d0c6febf52b555e6aa2f5c7d568ffb6
Ubed Patait <67995958+ubedullahpatait@users.noreply.github.com>
2021-07-06 21:12:01 +0530

    Update MessageDisplay.cs

--- BMICalculator/BMIResultDisplayLib/MessageDisplay.cs
+++ BMICalculator/BMIResultDisplayLib/MessageDisplay.cs
@@ -4,7 +4,7 @@ namespace BMIResultDisplayLib
 {
     public class MessageDisplay
     {
-       public void ConsoleDisplay(string message)
+       public void Display(string message)
         {
 
             Console.WriteLine(message);
