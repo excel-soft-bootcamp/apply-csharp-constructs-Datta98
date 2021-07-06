e81a9e863d0667ed88a42d6f0cff16e6ba817ca5
Ubed Patait <67995958+ubedullahpatait@users.noreply.github.com>
2021-07-06 23:54:40 +0530

    Update UserInputs.cs

--- BMICalculator/BMIUserInputLib/UserInputs.cs
+++ BMICalculator/BMIUserInputLib/UserInputs.cs
@@ -6,7 +6,7 @@ namespace BMIUserInputLib
     {
         public double GetUserHeight()
         {
-             ConsoleDisplayLib.ConsoleDisplayType.ConsoleDisplay("Enter your height in meter:");
+             ConsoleDisplayLib.ConsoleDisplayType.ConsoleDisplay("Enter your height in meter");
             string heightInMeter = Console.ReadLine();
             int height = int.Parse(heightInMeter);
             return height;
@@ -14,7 +14,7 @@ namespace BMIUserInputLib
 
         public double GetUserWeight()
         {
-             ConsoleDisplayLib.ConsoleDisplayType.ConsoleDisplay("Enter your weight in KG:");
+             ConsoleDisplayLib.ConsoleDisplayType.ConsoleDisplay("Enter your weight in KG");
             string weightInKG = Console.ReadLine();
             int weight = int.Parse(weightInKG);
             return weight; ;
