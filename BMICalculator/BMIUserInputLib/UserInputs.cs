43896cd36cc871b290b060125d15381ff2b581d6
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-07-06 15:56:43 +0530

    Add files via upload

---
+++ BMICalculator/BMIUserInputLib/UserInputs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BMIUserInputLib
+{
+    public class UserInputs
+    {
+        public double UserHeight()
+        {
+            double height = Convert.ToDouble(this.ReadFromConsole("Enter your height in meter:"));
+            return height;
+        }
+
+        public double UserWeight()
+        {
+            double weight = Convert.ToDouble(this.ReadFromConsole("Enter your weight in kg:"));
+            return weight;
+        }
+        private string ReadFromConsole(string message)
+        {
+
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            return input;
+        }
+
+
+    }
+}
