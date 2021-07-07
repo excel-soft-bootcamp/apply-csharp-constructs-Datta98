5ac70e947522c49a450376019b54cbd4ecdb8216
Ubed Patait <67995958+ubedullahpatait@users.noreply.github.com>
2021-07-07 09:04:26 +0530

    updated

---
+++ BMICalculatorApp/BMICalculatorApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+                BMIUserInputLib.ConsoleInputReader userInputs = new BMIUserInputLib.ConsoleInputReader();
+
+
+                BMIValueCalculationLib.CalculateBMI value = new BMIValueCalculationLib.CalculateBMI();
+                double BmiValue = value.BmiCalculation(userInputs.GetUserHeight(), userInputs.GetUserWeight());
+
+                BMIValueValidationLib.BmiValueValidation validate = new BMIValueValidationLib.BmiValueValidation();
+                string message = validate.CheckBmiValue(BmiValue);
+
+                BMIResultDisplayLib.MessageDisplay result = new BMIResultDisplayLib.MessageDisplay();
+                result.Display(message);
+                Console.ReadKey();
+
+            
+        }
+    }
+}
