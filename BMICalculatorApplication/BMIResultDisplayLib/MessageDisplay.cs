10010492f92e171f3d7bda43bb146a26035d808b
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-07-08 17:02:06 +0530

    Add files via upload

---
+++ BMICalculatorApplication/BMIResultDisplayLib/MessageDisplay.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BMIResultDisplayLib
+{
+    public class MessageDisplay
+    {
+       public void Display(string message)
+        {
+
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ BMICalculatorApplication/BMIValueValidationLib/BmiValueValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BMIValueValidationLib
+{
+    public class BmiValueValidation
+    {
+        public string CheckBmiValue(double BMIValue)
+        {
+            if (BMIValue < 18.5)
+            {
+                return "UnderWeight";
+            }
+            else if (BMIValue >= 25)
+            {
+                return "OverWeight";
+            }
+            else
+            {
+                return "Normal";
+            }
+        }
+
+    }
+}
